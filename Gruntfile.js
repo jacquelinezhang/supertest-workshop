d70697671688c8b01fd2aeaa0dd41ab76da918e3
juewen <jwezhang@thoughtworks.com>
2016-07-20 20:25:52 +0800

    add more scenarios

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
                 clearRequireCache: false
 
             },
-            src: ['test/*.js']
+            src: ['test/*books.js']
         }
      }
 
--- test/supertest-workshop_books.js
+++ test/supertest-workshop_books.js
@@ -1,17 +1,29 @@
 var request = require('supertest')
-var request = request('https://www.googleapis.com/books/v1/volumes?q=cucumber');
+var request = request('https://www.googleapis.com/books/v1/volumes');
+var bookname = 'cucumber'
+var maxResults = 1
 
+
+
+// Test Google Books API
 describe('GOOGLE BOOKS API',function(){
 
-    it('BOOKS API Get all book list - return 200', function(done){
+    // Get book list that name includes "cucumber", return http status 200 - OK
+    it('Get book list that name includes "cucumber", return http status 200 - OK', function(done){
 
         this.timeout(10000);
 
-        request.get('/')
+        request.get('?q='+bookname+'&maxResults='+maxResults)
             .expect(200)
             .end(function(err,res){
 
-                console.log(res);
+
+                console.log(res.body);
+
+                //How to print the info under items?
+                console.log(res.body.items.id);
+                console.log(res.body.items.selfLink);
+
                 console.log(err);
 
                 done(err);
