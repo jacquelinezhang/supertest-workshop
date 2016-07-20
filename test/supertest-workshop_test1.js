db292da47954f685857a66f2c7d5fb8ea748d2d2
Juewen Zhang <jwezhang@CNjwezhang.local>
2016-07-20 18:15:03 +0800

    init git

---
+++ test/supertest-workshop_test1.js
@@ -0,0 +1,23 @@
+var request = require('supertest')
+var request = request('https://www.googleapis.com/books/v1/volumes?q=cucumber');
+
+describe('GOOGLE BOOKS API',function(){
+
+    it('BOOKS API Get all book list - return 200', function(done){
+
+        this.timeout(10000);
+
+        request.get('/')
+            .expect(200)
+            .end(function(err,res){
+
+                console.log(res);
+                console.log(err);
+
+                done(err);
+
+            })
+
+    })
+
+})
---
+++ test/supertest-workshop_test2.js
@@ -0,0 +1,24 @@
+var request = require('supertest')
+var request = request('https://www.googleapis.com/books/v1/volumes?q=cucumber');
+
+describe('GOOGLE SHEETS API',function(){
+
+    it('BOOKS API Get all book list - return 200', function(done){
+
+        this.timeout(10000);
+
+        request.put('/')
+            .expect(200)
+            .end(function(err,res){
+
+                console.log(res);
+                console.log(err);
+
+
+                done(err);
+
+            })
+
+    })
+
+})
