b1310bee4121e9869323080a9afa32e19c84fbf7
juewen <jwezhang@thoughtworks.com>
2016-07-21 13:54:25 +0800

    add more scenarios

--- test/supertest-workshop_books.js
+++ test/supertest-workshop_books.js
@@ -1,9 +1,7 @@
 var request = require('supertest')
 var request = request('https://www.googleapis.com/books/v1/volumes');
 var bookname = 'cucumber'
-var maxResults = 1
-
-
+var maxResults = 2
 
 // Test Google Books API
 describe('GOOGLE BOOKS API',function(){
@@ -17,14 +15,42 @@ describe('GOOGLE BOOKS API',function(){
             .expect(200)
             .end(function(err,res){
 
+               console.log(res.body);
+
+                //How to print the info under items?
+//                console.log(res.body.items.id);
+//                console.log(res.body.items.selfLink);
+//                console.log(res.body.totalItems);
+//                console.log(res.body.items);
+
+//                console.log(err);
+
+                done(err);
+
+            })
+
+    })
+
+// Retrieve the id from last case, then use this id to get the book info
+
+    it('Get the first book by id, return http status 200 - OK', function(done){
+
+        this.timeout(10000);
+
+        request.get('/0dge3Xh6EjUC')
+            .expect(200)
+            .end(function(err,res){
+
 
                 console.log(res.body);
 
                 //How to print the info under items?
-                console.log(res.body.items.id);
-                console.log(res.body.items.selfLink);
+//                console.log(res.body.items.id);
+//                console.log(res.body.items.selfLink);
+//                console.log(res.body.totalItems);
+//                console.log(res.body.items);
 
-                console.log(err);
+//                console.log(err);
 
                 done(err);
 
