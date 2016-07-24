3034b3a1105b3257b383296b293e77f6215dafd5
juewen <jwezhang@thoughtworks.com>
2016-07-24 19:41:39 +0800

    add assert

--- test/supertest-workshop_books.js
+++ test/supertest-workshop_books.js
@@ -1,69 +1,22 @@
-var request = require('supertest')
-var request = request('https://www.googleapis.com/books/v1/volumes');
+var request = require('supertest')('https://www.googleapis.com/books/v1/volumes');
+var chai = require('chai');
+var expect = require('chai').expect;
 
 
 // Test Google Books API
 describe('GOOGLE BOOKS API - VOLUMES',function(){
 
-    var bookname = 'cucumber'
-    var maxResults = 2
     var id;
 
-    // Get book list that name includes "cucumber", return http status 200 - OK
-    it('Get book list that name includes "cucumber", return http status 200 - OK', function(done){
+    // Get book list that name includes "test", return http status 200 - OK
+    it('Get book list that name includes "test", return http status 200 - OK', function(done){
+        request.get('/?q=test')
 
-        this.timeout(10000);
-
-        request.get('/')
-            .query({
-                q: bookname,
-                maxResults: maxResults
-            })
             .expect(200)
-            .expect(function(res) {
 
-                var length = res.body.items.length;
-                length = maxResults;
-                console.log(length);
-////              res.body.name = res.body.name.toUpperCase();
-            })
             .end(function(err,res){
 
-               console.log(res.body.items[0].id);
-               id = res.body.items[0].id;
-
-//                console.log(err);
-
-                done(err);
-
-            })
-
-    })
-
-    // Get book list that name includes "cucumber", return http status 200 - OK
-    it('Get book list that name includes "cucumber", return http status 200 - OK', function(done){
-
-        this.timeout(10000);
-
-        request.get('/')
-            .query({
-                q: bookname,
-                maxResults: maxResults
-            })
-            .expect(200)
-            .expect(function(res) {
-                var length = res.body.items.length;
-                length = maxResults;
-                console.log(length);
-////              res.body.name = res.body.name.toUpperCase();
-            })
-            .end(function(err,res){
-
-               console.log(res.body.items[0].id);
-               id = res.body.items[0].id;
-
-//                console.log(err);
-
+//                console.log(res);
                 done(err);
 
             })
@@ -71,16 +24,60 @@ describe('GOOGLE BOOKS API - VOLUMES',function(){
     })
 
 
-// Retrieve the id from last case, then use this id to get the book info
 
+    // Get book list that name includes "cucumber", return http status 200 - OK
+    it('Get book list that name includes "cucumber", return http status 200 - OK', function(done){
+
+        var bookname = 'cucumber'
+        var maxResults = 1
+
+        this.timeout(10000);
+
+        request.get('/')
+
+            .query({
+                q: bookname,
+                maxResults: maxResults
+            })
+
+            .expect(200)
+
+            .expect(function(res) {
+
+                expect(res.body.items[0].volumeInfo.title.toLowerCase()).to.contain(bookname);
+                expect(res.body.items.length).be.at.most(maxResults);
+//                expect(res.body.items[0].id).to.includes(res.body.items[0].selfLink);
+                expect(res.body).to.have.any.keys('kind', 'totalItems', 'items');
+                expect(res.body).to.include.keys('kind', 'totalItems', 'items');
+
+            })
+
+            .end(function(err,res){
+
+               id = res.body.items[0].id;
+
+               done(err);
+
+            })
+
+    })
+
+
+    // Retrieve the id from last case, then use this id to get the book info
     it('Get the first book by id, return http status 200 - OK', function(done){
 
         this.timeout(10000);
 
         request.get('/' + id)
             .expect(200)
-            .end(function(err,res){
 
+            .expect(function(res){
+
+                expect(res.body.id).to.equal(id)
+
+            })
+
+            .end(function(err,res){
 
 //                console.log(res.body);
 //                console.log(err);
--- test/supertest-workshop_sheets.js
+++ test/supertest-workshop_sheets.js
@@ -1,41 +1,233 @@
-var request = require('supertest')
+var request = require('supertest')('https://sheets.googleapis.com/v4/spreadsheets');
+var chai = require('chai');
+var expect = require('chai').expect;
 
-var spreadsheetId = '1oWJuCpcrZEvi-rlnWqvl3bc1PTSbOw40NXbDp6kKWE4'
-var range = 'Sheet1!A1:D5'
-var request = request('https://sheets.googleapis.com/v4/spreadsheets/'+spreadsheetId+'/values/'+range);
-var accessToken = 'Bearer ya29.Ci8mA1FqoJcLtCKxwa0BoVyS6yCga74O3Se7D0FBXYUd9EbTrIDo9qdq5lFNo3Pt3Q'
+var spreadsheetId = '1gU8HQ72E7ECWqQYINSE2zhJlFiSpqMTVG3ShnzJQquE'
+var accessToken = 'Bearer ya29.CjAqA0vaA_0HE5UJeO9iBYRheTSFh9XHJI4_fGLQHsrbcSSSH-oAUN2KIfGFMsqs97g'
 
-var requestBody = {
-                     "range": "Sheet1!A1:D5",
-                     "majorDimension": "ROWS",
-                     "values": [
-                       ["Item", "Cost", "Stocked", "Ship Date"],
-                       ["Wheel", "$20.50", "4", "3/1/2016"],
-                       ["Door", "$15", "2", "3/15/2016"],
-                       ["Engine", "$100", "1", "30/20/2016"],
-                       ["Totals", "=SUM(B2:B4)", "=SUM(C2:C4)", "=MAX(D2:D4)"]
-                     ]
-                   }
+var sheetId;
+var sheetName = 'juewen';
+
+
+    var range = sheetName +'!A1:D4'
+    var requestBody = {
+                        "range": range,
+                        "majorDimension": "ROWS",
+                        "values": [
+                          ["Item", "Cost", "Stocked", "Ship Date"],
+                          ["Wheel", "$20.50", "4", "3/1/2016"],
+                          ["Door", "$15", "2", "3/15/2016"],
+                          ["Engine", "$100", "1", "30/20/2016"]
+                        ]
+                      }
 
 describe('GOOGLE SHEETS API',function(){
 
-    it('PUT a line to goole sheet - return 200', function(done){
+    it('Add a new sheet', function(done){
 
         this.timeout(10000);
 
-        request.put('?valueInputOption=USER_ENTERED')
+        var requestBody = {
+                            "requests": [
+                              {
+                                "addSheet": {
+                                  "properties": {
+                                    "title": sheetName
+                                  }
+                                }
+                              }
+                            ]
+                          }
+
+        request.post('/' + spreadsheetId + ':batchUpdate')
+
+        .set('Authorization', accessToken)
+        .set('Content-Type', 'application/json')
+
+        .send(requestBody)
+
+        .expect(200)
+
+        .expect(function(res){
+
+            expect(res.body.spreadsheetId).to.equal(spreadsheetId);
+            expect(res.body.replies[0].addSheet.properties.title).to.equal(sheetName);
+
+
+        })
+
+        .end(function(err, res){
+
+            sheetId = res.body.replies[0].addSheet.properties.sheetId;
+            done(err);
+
+        })
+
+    })
+
+
+    it('Write to multiple ranges', function(done){
+        this.timeout(10000);
+
+        var requestBody = {
+                            "valueInputOption": "USER_ENTERED",
+                            "data": [
+                              {
+                                "range": sheetName + "!A1:A4",
+                                "majorDimension": "COLUMNS",
+                                "values": [
+                                  ["AAAItem", "BBBWheel", "Door", "Engine"]
+                                ]
+                              },
+                              {
+                                "range": sheetName + "!B1:D2",
+                                "majorDimension": "ROWS",
+                                "values": [
+                                  ["Cost", "Stocked", "Ship Date"],
+                                  ["$20.50", "400", "3/1/2016"]
+                                ]
+                              }
+                            ]
+                          }
+
+        request.post('/'+ spreadsheetId +'/values:batchUpdate')
+
+        .set('Authorization', accessToken)
+        .set('Content-Type', 'application/json')
+
+        .send(requestBody)
+
+        .expect(200)
+        .expect(function(res){
+
+            expect(res.body.spreadsheetId).to.equal(spreadsheetId)
+            expect(res.body.responses.length).to.equal(2)
+
+            for (var i=0; i<res.body.responses.length; i++)
+            {
+
+                expect(res.body.responses[i].updatedRange).to.contain(sheetName);
+
+            }
+
+
+
+        })
+
+        .end(function(err,res){
+
+            done(err);
+
+        })
+
+    })
+
+    it('Write a single range', function(done){
+
+        this.timeout(10000);
+
+        request.put('/'+spreadsheetId+'/values/'+range)
+            .query({
+                valueInputOption:'USER_ENTERED'
+            })
+
             .set('Authorization', accessToken)
             .set('Content-Type', 'application/json')
             .send(requestBody)
             .expect(200)
 
+            .expect(function(res){
+
+                expect(res.body.spreadsheetId).to.equal(spreadsheetId)
+                expect(res.body.updatedRange).to.equal(range)
+
+
+            })
+
             .end(function(err,res){
 
-                console.log(res);
-                console.log(err);
                 done(err);
+                return range;
+                return requestBody;
             })
 
     })
+
+    it('Read a single range', function(done){
+        this.timeout(20000);
+
+        request.get('/'+spreadsheetId+'/values/'+range)
+
+
+            .set('Authorization', accessToken)
+
+            .expect(200)
+
+            .expect(function(res){
+
+                console.log(res.body);
+
+                console.log('------ requestBody-------');
+
+                console.log(requestBody);
+
+                console.log('-----end ------');
+
+                expect(res.body).to.deep.equal(requestBody);
+
+            })
+
+
+            .end(function(err, res){
+
+                done(err);
+
+            })
+
+
+    })
+
+
+
+    it('Delete a sheet', function(done){
+
+        this.timeout(20000)
+        console.log(sheetId)
+
+        var requestBody = {
+                            "requests": [
+                              {
+                                "deleteSheet": {
+                                  "sheetId": sheetId
+                                }
+                              }
+                            ]
+                          }
+
+        request.post('/' + spreadsheetId + ':batchUpdate')
+
+        .set('Authorization', accessToken)
+        .set('Content-Type', 'application/json')
+
+        .send(requestBody)
+
+        .expect(200)
+
+        .expect(function(res){
+
+            expect(res.body.spreadsheetId).to.equal(spreadsheetId);
+
+        })
+
+        .end(function(err,res){
+
+            console.log(res.body);
+            done(err);
+
+        })
+
+    })
+
+
 
 })
